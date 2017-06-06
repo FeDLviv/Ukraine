67178f21c84f2c1d1a84eb66d6c63bbf35f9f146
FeDLviv <fed.lviv@gmail.com>
2017-06-06 16:40:07 +0300

    Add background & navbar & animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $('tr[data-toggle="tooltip"]').tooltip({
-    placement: 'left'
+	placement: 'left'
 });
 
 $('[data-toggle="tooltip"]').tooltip();
+
+
+$('#main-nav').on('click', 'a', function (event) {
+	event.preventDefault();
+	var id = $(this).attr('href');
+	var top = $(id).offset().top;
+	$('body,html').animate({
+		scrollTop: top
+	}, 700);
+});
